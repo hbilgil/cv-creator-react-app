32dd8d980b8f02e2e9d42c6707f90ff3acc5b974
Halil Bilgil <hbilgil@gmail.com>
2022-09-19 13:33:42 +0300

    Edit App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">Gozalak
-    </div>
-  );
+  render() {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
