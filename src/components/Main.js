063b64f3eea45f4f3f2c509dfb3e819ebaf36f8a
Halil Bilgil <hbilgil@gmail.com>
2022-09-19 17:53:12 +0300

    Edit Main.js to import Form and Preview division components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,159 @@
 import React from 'react';
 import styled from 'styled-components';
+import uniqid from 'uniqid';
+import Form from './Form/Form';
+import Preview from './Preview/Preview';
 
 class Main extends React.Component {
-    render() {
-        return (
-            <MainWrapper>
 
-            </MainWrapper>
-          )
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      personalInfo: [
+        {
+          name: "firstName",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "lastName",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "address",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "website",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "email",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "phone",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "details",
+          value: "",
+          id: uniqid()
+        },
+      ],
+
+      educationInfo: [
+        {
+          name: "UniversityName1",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "universityDegree1",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "universityDate1",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "UniversityName2",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "universityDegree2",
+          value: "",
+          id: uniqid()
+        },
+        {
+          name: "universityDate2",
+          value: "",
+          id: uniqid()
+        },
+      ],
+
+      experienceInfo: [
+        { 
+          company: "",
+          position: "",
+          task: "",
+          start: "",
+          end: "",
+          id: uniqid(),
+        },
+      ],
+
+      skillsInfo: [
+        { 
+          skill: "",
+          id: uniqid()
+        },
+        { 
+          skill: "",
+          id: uniqid()
+        },
+      ],
+
+      newExperience: 
+        {  
+          company: "",
+          position: "",
+          task: "",
+          start: "",
+          end: "",
+          id: uniqid(),
+        },
+
+      newSkill: 
+        {
+          skill: "",
+          id: uniqid()
+        },
     }
+  }
+
+  render() {
+    return (
+      <MainWrapper>
+        <Form 
+          state={this.state}
+          personalInfo={this.state.personalInfo}
+          educationInfo={this.state.educationInfo}
+          experienceInfo={this.state.experienceInfo}
+          skillsInfo={this.state.skillsInfo} />
+        <Preview 
+          personalInfo={this.state.personalInfo}
+          educationInfo={this.state.educationInfo}
+          experienceInfo={this.state.experienceInfo}
+          skillsInfo={this.state.skillsInfo}
+        />
+      </MainWrapper>
+    )
+  }
 }
 
 const MainWrapper = styled.main`
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  gap: 4rem;
-  max-width: 1800px;
-  padding: 4rem 8rem;
-  margin: 0 auto;
-  margin-bottom: 4rem;
-  @media (max-width: 1600px) {
-    flex-direction: column;
-    align-items: center;
-  }
+display: flex;
+align-items: flex-start;
+justify-content: space-between;
+gap: 4rem;
+max-width: 1800px;
+padding: 4rem 8rem;
+margin: 0 auto;
+margin-bottom: 4rem;
+@media (max-width: 1600px) {
+  flex-direction: column;
+  align-items: center;
+}
 `
 
 export default Main;
