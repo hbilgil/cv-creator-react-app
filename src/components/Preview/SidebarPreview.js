c773d445076613cde3b21b919757b66e53d9164b
Halil Bilgil <hbilgil@gmail.com>
2022-09-20 14:08:37 +0300

    Edit Sidebar preview component to add new components

---
+++ src/components/Preview/SidebarPreview.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class SidebarPreview extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+  
+      }
+    }
+
+    render() {
+        return (
+            <SidebarContainer>
+                <InfoDiv>
+                    <InfoDivHeader>
+                    CONTACT
+                    </InfoDivHeader>
+                    <ContactDetailsDiv>
+                        <DetailDiv>
+                            <i className="fa-brands fa-internet-explorer"></i>
+                            <p>{this.props.personalInfo[3].value}</p>
+                        </DetailDiv>
+                        <DetailDiv>
+                            <i className="fa-solid fa-at"></i>
+                            <p>{this.props.personalInfo[4].value}</p>
+                        </DetailDiv>
+                        <DetailDiv>
+                            <i className="fa-solid fa-location-dot"></i>
+                            <p>{this.props.personalInfo[2].value}</p>
+                        </DetailDiv>
+                        <DetailDiv>
+                            <i className="fa-solid fa-phone"></i>
+                            <p>{this.props.personalInfo[5].value}</p>
+                        </DetailDiv>
+                    </ContactDetailsDiv>
+                </InfoDiv>
+                <InfoDiv>
+                    <InfoDivHeader>
+                    EDUCATION
+                    </InfoDivHeader>
+                    <DegreeDetailsDiv>
+                        <h4>{this.props.educationInfo[1].value}</h4>
+                        <h5>{this.props.educationInfo[0].value}</h5>
+                        <p>{this.props.educationInfo[2].value}</p>
+                    </DegreeDetailsDiv>
+                    <DegreeDetailsDiv>
+                        <h4>{this.props.educationInfo[4].value}</h4>
+                        <h5>{this.props.educationInfo[3].value}</h5>
+                        <p>{this.props.educationInfo[5].value}</p>
+                    </DegreeDetailsDiv>
+                </InfoDiv>
+                <InfoDiv>
+                    <InfoDivHeader>
+                    SKILLS
+                    </InfoDivHeader>
+                    <SkillList>
+                    {this.props.skillsInfo.map((skill) => {
+                        return <SkillListItem key={skill.id}>{skill.skill}</SkillListItem>
+                    })}
+                    </SkillList>
+                </InfoDiv>
+            </SidebarContainer>
+        )
+    }
+}
+
+const SidebarContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: ${({ theme }) => theme.colors.headerColor};
+    box-sizing: border-box;
+    width: 30%;
+    height: 80vh;
+    text-align: left;
+    padding: 35px 28px;
+    gap: 35px;
+`;
+
+const InfoDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const InfoDivHeader = styled.h3`
+    font-family: 'Teko', sans-serif;
+    font-size: 13px;
+    letter-spacing: 4px;
+    margin-bottom: 20px;
+`;
+
+const ContactDetailsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`;
+
+const DetailDiv = styled.div`
+    display: flex;
+`;
+
+const DegreeDetailsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const SkillList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    padding-left: 16px;
+    gap: 10px;
+`;
+
+const SkillListItem = styled.li`
+    font-weight: 400;
+    font-size: 11px;
+    &::marker {
+    font-size: 1.55em;
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export default SidebarPreview;
